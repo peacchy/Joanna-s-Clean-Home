70f4900d96629e770cbdf506a03a1bedd0a7defa
Daria Ratyńska <ratynskadaria@gmail.com>
2019-09-06 18:21:11 +0200

    create about us section

--- src/components/about-us/AboutUs.tsx
+++ src/components/about-us/AboutUs.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import "./AboutUs.scss";
 
 import Picture from "./picture/Picture";
+import Title from "./title/Title";
+import Text from "./text/Text";
 
 const AboutUs = () => {
 	return (
 		<div className="about_us">
-			<div className="about_us__picture">
-				<Picture />
+			<div className="about_us__title">
+				<Title />
+			</div>
+			<div className="about_us__content">
+				<div className="about_us__content__text">
+					<Text />
+				</div>
+				<div className="about_us__content__picture">
+					<Picture />
+				</div>
 			</div>
 		</div>
 	);
