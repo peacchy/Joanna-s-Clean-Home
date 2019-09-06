4988784cc06a705c4e7d5aa2899ab17b7e70e5c8
Daria Ratyńska <ratynskadaria@gmail.com>
2019-09-06 15:22:12 +0200

    create about_us image

---
+++ src/components/about-us/AboutUs.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Picture from "./picture/Picture";
+
+const AboutUs = () => {
+	return (
+		<div className="about_us">
+			<div className="about_us__picture">
+				<Picture />
+			</div>
+		</div>
+	);
+};
+
+export default AboutUs;
---
+++ src/components/about-us/picture/Picture.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Picture.scss';
+
+import Living_Room from '../../../img/img-aboutus/living_room.png';
+import Stars from '../../../img/img-aboutus/stars.png';
+
+
+const Picture = () => {
+    return (
+        <div className="picture">
+            <img className="picture__stars" src={Stars} />
+            <img className="picture__living_room" src={Living_Room} />
+            <div className="picture__square"/>
+        </div>
+    )
+}
+
+export default Picture;
